0308144cb62f0a0662c279c6d372b7a1b69a991d
bimeksf <d3disek@gmail.com>
2025-06-08 21:39:33 +0200

    Add SVG icons for Google, hamburger menu, and star
    - Added google.svg with the Google logo in color.
    - Added hamburger.svg for a simple hamburger menu icon.
    - Added star.svg featuring a star icon design.

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 const date = new Date()
 const footer = document.querySelector("footer")
 footer.textContent =   `© ${date.getFullYear()} Salon Elegance – Všechna práva vyhrazena`    
+
+
+const startC = document.getElementById("star")
+
+
+const stars  = Array.from({length:4},  () => "★").join("")
+
+
+
+startC.textContent= stars
+
+
+  const toggleBtn = document.getElementById("menu-toggle");
+  const menu = document.getElementById('menu');
+  const closeIcon = document.getElementById("close-icon");
+    const hamburgerIcon = document.getElementById("hamburger-icon");
+
+
+  toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+
+
+
+  });
